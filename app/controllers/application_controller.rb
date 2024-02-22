9136154496d04bf6bdde8a7c5712b012e14a6da2
yosyua0016 <159263548+yosyua0016@users.noreply.github.com>
2024-02-23 01:13:03 +0900

    新規登録

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  #before_action :authenticate_user!
+  #ログイン画面に遷移するように実装
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :encrypted_password,:name, :profile, :occupation, :position])
+  end
 end
