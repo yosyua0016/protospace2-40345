b2c5c43d362c6cfd6f793be4f8699fff43562dcc
yosyua0016 <159263548+yosyua0016@users.noreply.github.com>
2024-02-23 01:57:31 +0900

    ユーザー管理終了

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
 
   get 'prototypes', to: 'prototypes#index'
   resources :prototypes, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+
+  as :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy', as: :logout
+  end
 end
