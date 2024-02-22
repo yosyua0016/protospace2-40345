019103677d368427f21cc89824b6007c64ddee27
yosyua0016 <159263548+yosyua0016@users.noreply.github.com>
2024-02-23 00:05:08 +0900

    1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root to: "prototypes#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'prototypes', to: 'prototypes#index'
+  resources :prototypes, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 end
