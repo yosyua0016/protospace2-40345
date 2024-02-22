04eab792d5da18f0588bf9aa1faed94c70a62343
yosyua0016 <159263548+yosyua0016@users.noreply.github.com>
2024-02-23 06:08:25 +0900

    投稿したプロトタイプがトップページで表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,19 @@
 class ApplicationController < ActionController::Base
-  #before_action :authenticate_user!
-  #ログイン画面に遷移するように実装
+  # Deviseコントローラーの場合に許可されるパラメーターを設定
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # アクティブストレージのURLオプションを設定
+  before_action :set_url_options
+
   private
+
+  # Deviseで許可されるパラメーターを設定
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :name, :profile, :occupation, :position])
   end
-end
 
+  # アクティブストレージのURLオプションを設定
+  def set_url_options
+    ActiveStorage::Current.url_options = Rails.application.routes.default_url_options
+  end
+end
